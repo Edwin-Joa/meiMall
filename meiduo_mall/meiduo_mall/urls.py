c61ca9927d779ed27ff829b397ddd4349031717e
EdwinZ <2437458978@qq.com>
2020-06-20 23:40:30 +0800

    创建验证功能子应用verifications，并注册子应用

--- meiduo_mall/meiduo_mall/urls.py
+++ meiduo_mall/meiduo_mall/urls.py
@@ -25,4 +25,5 @@ register_converter(MobileConverter,'mobile')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('apps.users.urls')),
+    path('',include('apps.verifications.urls')),
 ]

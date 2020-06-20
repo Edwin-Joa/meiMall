dd183da17eeed00e35036ae45dd2ce5942735ffc
EdwinZ <2437458978@qq.com>
2020-06-20 23:30:01 +0800

    实现用户名重复注册功能，并修复了转换器的bug

---
+++ meiduo_mall/apps/users/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.views import View
+from django.http import JsonResponse
+from .models import User
+import logging
+logger = logging.getLogger('django')
+# Create your views here.
+
+
+class UsernameCountView(View):
+    def get(self,request,username):
+        try:
+            count = User.objects.filter(username=username).count()
+        except Exception as e:
+            logger.info('连接数据库失败')
+            return JsonResponse({'code':400,'errmsg':'数据库访问失败'})
+
+        return JsonResponse({'code':0,'errmsg':'ok','count':count})
+
---
+++ meiduo_mall/meiduo_mall/utils/converters.py
@@ -0,0 +1,20 @@
+
+
+class UsernameConverter:
+    regex = '[a-zA-Z0-9_-]{5,20}'
+
+    def to_python(self, value):
+        return str(value)
+
+    def to_url(self, value):
+        return str(value)
+
+
+class MobileConverter:
+    regex = '1[3-9]\d{9}'
+
+    def to_python(self, value):
+        return int(value)
+
+    def to_url(self, value):
+        return str(value)
